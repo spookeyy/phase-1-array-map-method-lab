d2fa85cf92b333ae3a4b6eeed03c5b7c44bbad69
spookeyy <pangasmeshack@gmail.com>
2024-03-30 22:14:20 +0300

    title casing words

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
 const titleCased = () => {
-  return tutorials
+  // return tutorials
+  //  arr = tutorials.map(str =>  str.charAt(0).toUpperCase() + str + str.slice(1));
+  let tutor = []
+  let caps = tutorials.map(string => {
+    tutor = string.split(' ').map(str => str.charAt(0).toUpperCase() + str.slice(1))
+   return tutor.join(' ')})
+   //debugger
+    return caps
+
 }
+
+console.log(titleCased())
